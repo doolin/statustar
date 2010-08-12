61d3af6c7cbe97c931e21e33c86211f01708e8fa
walteryu <kharma@gmail.com>
2010-08-11 19:46:09 -0700

    Status Star Ruby

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :statuses
+
+  map.resources :statuses
+
+  map.resources :statuses
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
