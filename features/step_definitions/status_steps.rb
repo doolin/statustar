3b7ae5dd8c3e8cd31bc539415399f1bcbc200e74
Dave Doolin <david.doolin@gmail.com>
2011-09-20 08:55:02 -0700

    Status updating feature now underway.

--- features/step_definitions/status_steps.rb
+++ features/step_definitions/status_steps.rb
@@ -1,7 +1,19 @@
 
 Given /^the user's status is "([^"]*)"$/ do |arg1|
-  visit(status_path)
-  pending # express the regexp above with the code you wish you had
+  #response_body.should have_selector 'input[type=radio][checked=checked][value=information]'
+  visit('/statuses/new')
+  #input("#busy").should be_checked
+  choose('status_state_1')
+  find_field('status_state_1').should be_checked
+  
+  
+  field_checked = find_field('status_state_1')['checked']
+  if field_checked.respond_to? :should
+    field_checked.should be_true
+  else
+    assert field_checked
+  end
+  #pending # express the regexp above with the code you wish you had
 end
 
 When /^the user clicks "([^"]*)"$/ do |arg1|
