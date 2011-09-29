cbcb8fb9485e750ddeeb02500201b49319abc720
Dave Doolin <david.doolin@gmail.com>
2011-09-29 08:53:22 -0700

    Added specs for getting statuses shown properly.

---
+++ spec/requests/statuses_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "Statuses" do
+  describe "GET /statuses" do
+    
+    # This should not work, only :create, :destroy for microposts
+    xit "works! (now write some real specs)" do
+      get statuses_path
+    end
+  end
+end
