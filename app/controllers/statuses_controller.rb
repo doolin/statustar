df7d8a947b2c2c4de5173bb361d6347e4ed4a77f
Dave Doolin <david.doolin@gmail.com>
2011-10-05 09:05:27 -0700

    Cleaning up Destroy handling code in Status.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -89,7 +89,8 @@ class StatusesController < ApplicationController
     @status.destroy
 
     respond_to do |format|
-      format.html { redirect_to(statuses_url) }
+      #format.html { redirect_to(statuses_url) }
+      format.html { redirect_to(root_path) }
       format.xml  { head :ok }
     end
   end
