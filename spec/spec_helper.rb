7019f934c7654c89beca77d168514e3db20274b3
Walter Yu <kharma@gmail.com>
2011-12-31 18:49:40 -0800

    Comment out spork.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 require 'rubygems'
-require 'spork'
+# require 'spork'
 
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+# Spork.prefork do
+#   # Loading more in this block will cause your tests to run faster. However, 
+#   # if you change any configuration or code from libraries loaded here, you'll
+#   # need to restart spork for it take effect.
   
-end
+# end
 
-Spork.each_run do
-  # This code will be run each time you run your specs.
+# Spork.each_run do
+#   # This code will be run each time you run your specs.
   
-end
+# end
 
 # --- Instructions ---
 # - Sort through your spec_helper file. Place as much environment loading 
