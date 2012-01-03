10e5855d583093fdcfa3b40198e6d6dc1a11268b
Walter Yu <kharma@gmail.com>
2012-01-02 16:42:09 -0800

    Remove duplication application layout partial.

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
-describe 'layouts/_application.html.haml' do
+describe 'layouts/application.html.haml' do
 
 	before do
 		render
 	end
 
-	it "should render head and body" do
-		rendered.should have_selector 'div',
+	it "should render head, title, body and container tags" do
+		rendered.should have_selector "head"
+		rendered.should have_selector "title"
+		rendered.should have_selector "body"
+		rendered.should have_selector "div",
 			:class => 'container'
 	end
 	
-	xit "should render title" do 
-		rendered.should have_tag "title", 
-			:id => title
-	end
-
 	it "should render header and footer partials" do
 		view.should render_template :partial => "_header"
 		view.should render_template :partial => "_footer"
