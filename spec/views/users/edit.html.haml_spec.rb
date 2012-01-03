0c63df75c5771e39a2c9f1a9553b83e93e5a9c88
Dave Doolin <david.doolin@gmail.com>
2012-01-02 20:00:17 -0800

    added haml spec for edit, chmod to 644

--- spec/views/users/edit.html.haml_spec.rb
+++ spec/views/users/edit.html.haml_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
-describe "users/new.html.erb" do
+describe "users/edit.html.haml" do
   before(:each) do
-    assign(:user, stub_model(User,
+    @user = assign(:user, stub_model(User,
+      :new_record? => false,
       :name => "MyString",
       :email => "MyString"
-    ).as_new_record)
+    ))
   end
 
-  it "renders new user form" do
+  it "renders the edit user form" do
     render
 
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
-    assert_select "form", :action => users_path, :method => "post" do
+    assert_select "form", :action => user_path(@user), :method => "post" do
       assert_select "input#user_name", :name => "user[name]"
       assert_select "input#user_email", :name => "user[email]"
     end
