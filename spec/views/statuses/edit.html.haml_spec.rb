c2dc5c6a1bce2a95fe94f1926f2c5c0f401386ce
Walter Yu <kharma@gmail.com>
2012-01-06 12:09:37 -0800

    Editing statuses edit view spec.

--- spec/views/statuses/edit.html.haml_spec.rb
+++ spec/views/statuses/edit.html.haml_spec.rb
@@ -2,9 +2,31 @@ require 'spec_helper'
 
 describe "statuses/edit.html.haml" do
 
-	xit "should render Show and Back text" do
-    render :template => "layouts/application"
+	it "should render Show and Back text" do
+    render :template => "layouts/application.html.haml"
     render :partial => "statuses/form.html.haml"
-		rendered.should =~ /Show/
+		rendered.should =~ /Busy/
+    rendered.should =~ /Maybe/
+    rendered.should =~ /Open/
 	end
+ 
+  it "should infer the controller path" do
+    controller.request.path_parameters["controller"].should eq('statuses')
+  end
+ 
+  it "should infer the controller action" do
+    controller.request.path_parameters["action"].should eq('edit')
+  end
+ 
+  it "should display edit statuses" do
+    render :template => "layouts/application.html.haml"
+    rendered.should =~ /Status/
+  end
+ 
+  it "should have correct <title> element " do
+    render :template => "layouts/application.html.haml", 
+      :layout => "layouts/application"
+    rendered.should have_selector "title", 
+      :content => "Statustar"
+  end
 end
