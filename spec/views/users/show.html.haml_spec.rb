3591dec37a26c69935340ec474b2dee64a92d93b
Walter Yu <kharma@gmail.com>
2012-01-06 12:56:22 -0800

    Debugging follow/unfollow user view and remove webrat comments.

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -11,9 +11,7 @@ describe "users/show.html.haml" do
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
     rendered.should match(/Name/)
-    # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
     #rendered.should match(/Email/)
   end
   
