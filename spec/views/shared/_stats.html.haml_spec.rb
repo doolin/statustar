bb044bd5db292a20e7e9532796425784e510876e
Walter Yu <kharma@gmail.com>
2012-01-08 18:34:49 -0800

    Add shared views stats haml and spec.

---
+++ spec/views/shared/_stats.html.haml_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "shared/_stats.html.haml" do
+  
+  it "should infer the controller path" do
+    controller.request.path_parameters["controller"].should eq('shared')
+  end
+ 
+  it "should render head, title and body tags" do
+    render :template => "layouts/application.html.haml"
+    rendered.should have_selector "head"
+    rendered.should have_selector "title"
+    rendered.should have_selector "body"
+  end
+end
