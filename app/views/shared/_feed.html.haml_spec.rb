bbe2eb1994ad596ab884e807efdea634c181d980
Walter Yu <kharma@gmail.com>
2012-01-08 18:15:31 -0800

    Add shared views feed haml and spec.

---
+++ app/views/shared/_feed.html.haml_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "shared/_feed.html.haml" do
+  
+  it "should infer the controller path" do
+    controller.request.path_parameters["controller"].should eq('shared')
+  end
+ 
+  it "should render head, title and body tags" do
+    render :template => "layouts/application.html.haml"
+    rendered.should have_selector "head"
+    rendered.should have_selector "title"
+    rendered.should have_selector "body"
+
+  end
+end
