406d45f3b0a2e6ee3e9016981b19187dd980d916
Dave Doolin <david.doolin@gmail.com>
2012-01-29 20:27:01 -0800

    added some social buttons.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,12 @@
 //= require foundation
 //= require_self
 //= require_tree .
-= require updater
+//= require updater
 //= require status
 
+// For Google+
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
