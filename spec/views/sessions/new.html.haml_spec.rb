6c8683f437da7f3e34313b34fda456b5c15975ae
Dave Doolin <david.doolin@gmail.com>
2015-02-28 07:03:58 -0800

    update sessions view specs

--- spec/views/sessions/new.html.haml_spec.rb
+++ spec/views/sessions/new.html.haml_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe "sessions/new.html.haml" do
-
+describe "sessions/new" do
   it "should infer the controller path" do
     controller.request.path_parameters["controller"].should eq('sessions')
   end
@@ -11,7 +10,7 @@ describe "sessions/new.html.haml" do
   end
  
   it "should render head, title and body tags" do
-    render :template => "layouts/application.html.haml"
+    render :template => "layouts/application"
     rendered.should have_selector "head"
     rendered.should have_selector "title"
     rendered.should have_selector "body"
