16f734cbc6c2eebab98dc43b222647a3d4491ea3
Dave Doolin <david.doolin@gmail.com>
2015-03-08 15:38:49 -0700

    expect syntax for status model spec

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,61 +1,54 @@
 require 'spec_helper'
 
 describe Status do
-
   before(:each) do
     @user = FactoryGirl.create(:user)
     @attr = { :state => 1 }
   end
 
-  # TODO: Write up a little blog post on how
-  # the error returned by     @user.status.create!(@attr)
-  it "should create a new instance given valid attributes" do
+  it "creates a new instance given valid attributes" do
     @user.statuses.create!(@attr)
   end
 
   describe "user associations" do
-    
     before(:each) do
       @status = @user.statuses.create(@attr)
     end
 
-    it "should have a user attribute" do
-      @status.should respond_to(:user)
+    it "has a user attribute" do
+      expect(@status).to respond_to(:user)
     end
     
-    it "should have the right associated user" do
-      @status.user_id.should == @user.id
-      @status.user.should == @user
+    it "has the right associated user" do
+      expect(@status.user).to eq @user
     end
   end
 
   describe "validations" do
-
-    it "should require a user id" do
-      Status.new(@attr).should_not be_valid
+    it "requires a user id" do
+      expect(Status.new(@attr)).not_to be_valid
     end
 
-    it "should require nonblank content" do
-      @user.statuses.build(:state => "  ").should_not be_valid
+    it "requires nonblank content" do
+      expect(@user.statuses.build(:state => "  ")).not_to be_valid
     end
 
-    it "should reject long content" do
-      @user.statuses.build(:state => "a" * 141).should_not be_valid
+    it "rejects long content" do
+      expect(@user.statuses.build(:state => "a" * 141)).not_to be_valid
     end
   end
 
-  it "should return the correct state" do
+  it "returns the correct state" do
     status = Status.new(:state => 1)
-    status.find_active(1).should eq("active")
+    expect(status.find_active(1)).to eq "active"
   end
   
-  it "should return inactive correctly" do
+  it "returns inactive correctly" do
     status = Status.new(:state =>2)
-    status.find_active(1).should eq("inactive")
+    expect(status.find_active(1)).to eq("inactive")
   end
 
   describe "from_users_followed_by" do
-
     before(:each) do
       @other_user = FactoryGirl.create(:user, :email => FactoryGirl.generate(:email))
       @third_user = FactoryGirl.create(:user, :email => FactoryGirl.generate(:email))
@@ -67,22 +60,20 @@ describe Status do
       @user.follow!(@other_user)
     end
 
-    it "should have a from_users_followed_by class method" do
-      Status.should respond_to(:from_users_followed_by)
+    it "has a from_users_followed_by class method" do
+      expect(Status).to respond_to(:from_users_followed_by)
     end
 
-    it "should include the followed user's statuses" do
-      Status.from_users_followed_by(@user).should include(@other_post)
+    it "includes the followed user's statuses" do
+      expect(Status.from_users_followed_by(@user)).to include(@other_post)
     end
 
-    it "should include the user's own statuses" do
-      Status.from_users_followed_by(@user).should include(@user_post)
+    it "includes the user's own statuses" do
+      expect(Status.from_users_followed_by(@user)).to include(@user_post)
     end
 
-    it "should not include an unfollowed user's statuses" do
-      Status.from_users_followed_by(@user).should_not include(@third_post)
+    it "does not include an unfollowed user's statuses" do
+      expect(Status.from_users_followed_by(@user)).not_to include(@third_post)
     end
-
   end
-
 end
