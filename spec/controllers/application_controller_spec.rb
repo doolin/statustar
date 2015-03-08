7b803c1396845cf5a660af692b2b041c61fe543f
Dave Doolin <david.doolin@gmail.com>
2015-03-08 15:49:56 -0700

    working on view tests

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  # This is currently a dumb test moved over from the application template
+  # view test. RSpec 3 does not support render_template in views, so the
+  # test is moved here. Would be nice to remember what we were trying to
+  # test for when we wrote the test the first time.
+  it "should render header, footer and stylesheets partials" do
+    render_template :partial => "_header"
+    render_template :partial => :footer
+  end
+end
+
--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -1,25 +1,27 @@
 require 'spec_helper'
 
 describe 'layouts/application' do
-before do
-render
-end
+  before do
+    render
+  end
 
-it "should render head, title, body and container tags" do
-rendered.should have_selector "head"
-rendered.should have_selector "title"
-rendered.should have_selector "body"
-rendered.should have_selector "div", :class => 'container'
-end
+  it "should render head, title, body and container tags" do
+    rendered.should have_selector "head"
+    rendered.should have_selector "title"
+    rendered.should have_selector "body"
+    rendered.should have_selector "div", :class => 'container'
+  end
 
-# `render_template` is available in controller and request specs now,
-# no longer available in view specs
-xit "should render header, footer and stylesheets partials" do
-view.should render_template :partial => "_header"
-view.should render_template :partial => :footer
-end
+  # `render_template` is available in controller and request specs now,
+  # no longer available in view specs. A better way to write this test
+  # is to figure out what we're looking for, and test for that.
+  # Otherwise this is more or less a framework test, and not a smart one.
+  xit "should render header, footer and stylesheets partials" do
+    view.should render_template :partial => "_header"
+    view.should render_template :partial => :footer
+  end
 
-it "should render javascript" do
-rendered.should have_selector "script"
-end
+  it "should render javascript" do
+    rendered.should have_selector "script"
+  end
 end
