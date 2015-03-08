e962e80180d4dec73a0653ac581e489660a2e7c4
Dave Doolin <david.doolin@gmail.com>
2015-03-08 16:32:51 -0700

    moved secret key to env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,2 @@
-# Be sure to restart your server when you modify this file.
+Statustar::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Statustar::Application.config.secret_token = '3858e1c46b7e861ff37c61af16e392f663937b51666d947a9c26d138e99c902815f92c79798aa90a291c9d37a9032bc22b8df07e9071a94fe18fde03f0015db4'
