a5bcb9cb072dcc54d31232ef2359edef600d04f7
David Doolin <david.doolin@gmail.com>
2015-03-26 05:21:20 -0700

    more spec fixes for views and features

--- spec/views/layouts/statuses.html.haml_spec.rb
+++ spec/views/layouts/statuses.html.haml_spec.rb
@@ -1,28 +1,15 @@
 require 'spec_helper'
 
-describe 'layouts/statuses', type: :view do
-it "should render head, title, body and container tags", type: :view do
-render
-rendered.should have_selector "head"
-rendered.should have_selector "title"
-rendered.should have_selector "body"
-end
-	
-it "displays all the statuses", type: :view do
-assign(:statuses, [
-double(Status, :name => "busy"),
-double(Status, :name => "maybe"),
-double(Status, :name => "open"),
-])
-render
-rendered.should =~ /busy/
-rendered.should =~ /maybe/
-rendered.should =~ /open/
-end
-
-it "should render nav and style tags", type: :view do
-render
-rendered.should have_selector "nav"
-rendered.should have_selector "style"
-end
+describe 'layouts/statuses' do
+  it "displays all the statuses" do
+    assign(:statuses, [
+      double(Status, :name => "busy"),
+      double(Status, :name => "maybe"),
+      double(Status, :name => "open"),
+    ])
+    render
+    rendered.should =~ /busy/
+    rendered.should =~ /maybe/
+    rendered.should =~ /open/
+  end
 end
