4b352ea0f98d5e7c8a1747af57fde6cba6c28fe1
David Doolin <david.doolin@gmail.com>
2017-03-05 07:26:09 -0800

    user model spec - start cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,39 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @attr = {
+   let(:attr) do
+     {
       name: 'Example User',
       email: 'user@example.com',
       password: 'foobar',
       password_confirmation: 'foobar'
     }
-  end
+   end
 
   it 'creates a new instance given valid attributes' do
-    User.create!(@attr)
+    User.create!(attr)
   end
 
   it 'requires a name' do
-    no_name_user = User.new(@attr.merge(name: ''))
+    no_name_user = User.new(attr.merge(name: ''))
     expect(no_name_user).not_to be_valid
   end
 
   it 'requires an email address' do
-    no_email_user = User.new(@attr.merge(email: ''))
+    no_email_user = User.new(attr.merge(email: ''))
     expect(no_email_user).to_not be_valid
   end
 
   it 'should reject names that are too long' do
     long_name = 'a' * 51
-    long_name_user = User.new(@attr.merge(name: long_name))
+    long_name_user = User.new(attr.merge(name: long_name))
     expect(long_name_user).not_to be_valid
   end
 
   it 'should accept valid email addresses' do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(email: address))
+      valid_email_user = User.new(attr.merge(email: address))
       expect(valid_email_user).to be_valid
     end
   end
@@ -41,50 +41,50 @@ describe User do
   it 'should reject invalid email addresses' do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(email: address))
+      invalid_email_user = User.new(attr.merge(email: address))
       expect(invalid_email_user).not_to be_valid
     end
   end
 
   it 'should reject duplicate email addresses' do
     # Put a user with given email address into the database.
-    User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
+    User.create!(attr)
+    user_with_duplicate_email = User.new(attr)
     expect(user_with_duplicate_email).not_to be_valid
   end
 
   it 'should reject email addresses identical up to case' do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(email: upcased_email))
-    user_with_duplicate_email = User.new(@attr)
+    upcased_email = attr[:email].upcase
+    User.create!(attr.merge(email: upcased_email))
+    user_with_duplicate_email = User.new(attr)
     expect(user_with_duplicate_email).not_to be_valid
   end
 
   describe 'password validations' do
     it 'requires a password' do
-      expect(User.new(@attr.merge(password: '', password_confirmation: ''))).not_to be_valid
+      expect(User.new(attr.merge(password: '', password_confirmation: ''))).not_to be_valid
     end
 
     it 'should require a matching password confirmation' do
-      expect(User.new(@attr.merge(password_confirmation: 'invalid'))).to_not be_valid
+      expect(User.new(attr.merge(password_confirmation: 'invalid'))).to_not be_valid
     end
 
     it 'should reject short passwords' do
       short = 'a' * 5
-      hash = @attr.merge(password: short, password_confirmation: short)
+      hash = attr.merge(password: short, password_confirmation: short)
       expect(User.new(hash)).not_to be_valid
     end
 
     it 'should reject long passwords' do
       long = 'a' * 41
-      hash = @attr.merge(password: long, password_confirmation: long)
+      hash = attr.merge(password: long, password_confirmation: long)
       expect(User.new(hash)).not_to be_valid
     end
   end
 
   describe 'password encryption' do
     before(:each) do
-      @user = User.create!(@attr)
+      @user = User.create!(attr)
     end
 
     it 'should have an encrypted password attribute' do
@@ -97,7 +97,7 @@ describe User do
 
     describe 'has_password? method' do
       it 'should be true if the passwords match' do
-        expect(@user.password?(@attr[:password])).to be true
+        expect(@user.password?(attr[:password])).to be true
       end
 
       it "should be false if the passwords don't match" do
@@ -107,17 +107,17 @@ describe User do
 
     describe 'authenticate method' do
       it 'should return nil on email/password mismatch' do
-        wrong_password_user = User.authenticate(@attr[:email], 'wrongpass')
+        wrong_password_user = User.authenticate(attr[:email], 'wrongpass')
         expect(wrong_password_user).to be_nil
       end
 
       it 'should return nil for an email address with no user' do
-        nonexistent_user = User.authenticate('bar@foo.com', @attr[:password])
+        nonexistent_user = User.authenticate('bar@foo.com', attr[:password])
         expect(nonexistent_user).to be_nil
       end
 
       it 'should return the user on email/password match' do
-        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user = User.authenticate(attr[:email], attr[:password])
         expect(matching_user).to eq @user
       end
     end
@@ -125,7 +125,7 @@ describe User do
 
   describe 'status associations' do
     before(:each) do
-      @user = User.create(@attr)
+      @user = User.create(attr)
       @s1 = FactoryGirl.create(:status, user: @user, created_at: 1.day.ago)
       @s2 = FactoryGirl.create(:status, user: @user, created_at: 1.hour.ago)
     end
