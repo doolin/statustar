43e3613ec4c2ca06b9f95245d99e3788cfb67505
David Doolin <david.doolin@gmail.com>
2017-03-21 07:53:48 -0700

    rspec 3 syntax for UserController
    Went through this manually and updated the example text as well.
    
    Some of the specs need to move to view specs or be deleted.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,207 +4,215 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
-  describe 'GET :show' do
+  describe '.show' do
     before(:each) do
       @user = FactoryGirl.create(:user)
       test_sign_in(@user)
     end
 
-    it 'should be successful' do
+    it 'succeeds' do
       get :show, id: @user
-      response.should be_success
+      expect(response).to be_success
     end
 
-    it 'should find the right user' do
+    it 'finds the right user' do
       get :show, id: @user
-      assigns(:user).should == @user
+      expect(assigns(:user)).to eq @user
     end
 
-    it 'should have the right title' do
+    it 'has the right title' do
       get :show, id: @user
       expect(response.body).to match(/#{@user.name}/)
-      # response.should have_selector("title", :text => @user.name)
     end
 
     # Yes this spec is currently redundant with respect to the
     # spec above. It's not supposed to be.
-    it "should include the user's name" do
+    it "includes the user's name" do
       get :show, id: @user
       expect(response.body).to match(/#{@user.name}/)
-      # response.should have_selector("h1", :text => @user.name)
     end
 
-    it "should show the user's statuses", type: :feature do
+    # TODO: move or delete
+    it "shows the user's statuses", type: :feature do
       mp1 = FactoryGirl.create(:status, user: @user)
       mp2 = FactoryGirl.create(:status, user: @user)
       get :show, id: @user
       expect(response.body).to match(/#{mp1.state}/)
       expect(response.body).to match(/#{mp2.state}/)
-      # response.should have_selector("span.content", :text => mp1.content)
-      # response.should have_selector("span.content", :text => mp2.content)
     end
   end
 
-  describe 'GET :new' do
-    xit 'should have a name field' do
+  describe '.new' do
+    it 'succeeds' do
       get :new
-      response.should have_selector("input[name='user[name]'][type='text']")
+      expect(response).to be_success
     end
 
-    xit 'should have an email field' do
-      get :new
-      response.should have_selector("input[name='user[email]'][type='text']")
-    end
-
-    xit 'should have a password field' do
-      get :new
-      response.should have_selector("input[name='user[password]'][type='password']")
-    end
-
-    xit 'should have a password confirmation field' do
-      get :new
-      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
-    end
-
-    xit 'should be successful' do
-      get :new
-      response.should be_success
-    end
-
-    xit 'should have the right title' do
-      get :new
-      response.should have_selector('title', text: 'Sign up')
-    end
-
-    it 'as a signed in user you should be redirect to root url' do
+    it 'redirects to root url when signed in' do
       @user = FactoryGirl.create(:user)
       test_sign_in(@user)
       get :new
-      response.should redirect_to(root_path)
+      expect(response).to redirect_to(root_path)
+    end
+
+    # TODO: move into view spec or delete
+    xit 'has a name field' do
+      get :new
+      expect(response).to have_selector("input[name='user[name]'][type='text']")
+    end
+
+    xit 'has an email field' do
+      get :new
+      expect(response).to have_selector("input[name='user[email]'][type='text']")
+    end
+
+    xit 'has a password field' do
+      get :new
+      expect(response).to have_selector("input[name='user[password]'][type='password']")
+    end
+
+    xit 'has a password confirmation field' do
+      get :new
+      expect(response).to have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+
+    xit 'has the right title' do
+      get :new
+      expect(response).to have_selector('title', text: 'Sign up')
     end
   end
 
-  describe 'POST :create' do
+  describe '.create' do
     describe 'failure' do
       before(:each) do
         @attr = { name: '', email: '', password: '', password_confirmation: '' }
       end
 
-      it 'should not create a user' do
-        lambda do
+      it 'does not create a user' do
+        expect do
           post :create, user: @attr
-        end.should_not change(User, :count)
+        end.not_to change(User, :count)
       end
 
-      xit 'should have the right title' do
+      # TODO: move to view spec or delete
+      xit 'has the right title' do
         post :create, user: @attr
         response.should have_selector('title', text: 'Sign up')
       end
 
-      it 'should render the :new page' do
+      it 'renders the signup page' do
         post :create, user: @attr
-        response.should render_template('new')
+        expect(response).to render_template('new')
       end
     end
 
-    describe 'success' do
+    context 'success' do
       before(:each) do
         @attr = { name: 'New User', email: 'user@example.com',
                   password: 'foobar', password_confirmation: 'foobar' }
       end
 
-      it 'should create a user' do
-        lambda do
+      it 'creates a user' do
+        expect do
           post :create, user: @attr
-        end.should change(User, :count).by(1)
+        end.to change(User, :count).by(1)
       end
 
-      it 'should sign the user in' do
+      it 'signs the user in' do
         post :create, user: @attr
-        controller.should be_signed_in
+        expect(controller).to be_signed_in
       end
 
-      it 'should redirect to the user :show page' do
+      it "redirects to the user's page" do
         post :create, user: @attr
-        response.should redirect_to(user_path(assigns(:user)))
+        expect(response).to redirect_to(user_path(assigns(:user)))
       end
 
-      it 'should have a welcome message' do
+      it 'displays a welcome message' do
         post :create, user: @attr
-        flash[:success].should =~ /welcome to statustar/i
+        expect(flash[:success]).to match(/welcome to statustar/i)
       end
     end
 
-    it 'as a signed in user you should be redirect to root url' do
+    it 'redirects to root url after signing in' do
       @user = FactoryGirl.create(:user)
       test_sign_in(@user)
       post :create # No need to send any attributes, shouldn't get that far ...
-      response.should redirect_to(root_path)
+      expect(response).to redirect_to(root_path)
     end
   end
 
-  describe 'GET :edit' do
+  describe '.edit' do
     before(:each) do
       @user = FactoryGirl.create(:user)
       test_sign_in(@user)
     end
 
-    it 'should be successful' do
+    it 'succeeds' do
       get :edit, id: @user
-      response.should be_success
+      expect(response).to be_success
     end
 
-    xit 'should have the right title' do
+    # TODO: move to view spec or delete
+    xit 'has the right title' do
       get :edit, id: @user
-      response.should have_selector('title', text: 'Edit user')
+      expect(response).to have_selector('title', text: 'Edit user')
     end
   end
 
-  describe "PUT 'update'" do
+  describe '.update' do
     before(:each) do
       @user = FactoryGirl.create(:user)
       test_sign_in(@user)
     end
 
-    describe 'failure' do
+    context 'failure' do
       before(:each) do
-        @attr = { email: '', name: '', password: '',
-                  password_confirmation: '' }
+        @attr = {
+          email: '',
+          name: '',
+          password: '',
+          password_confirmation: ''
+        }
       end
 
-      it "should render the 'edit' page" do
+      it "renders the edit page" do
         put :update, id: @user, user: @attr
-        response.should render_template('edit')
+        expect(response).to render_template('edit')
       end
 
-      it 'should have the right title' do
+      # TODO: move to view spec or delete
+      it 'has the right title' do
         put :update, id: @user, user: @attr
         expect(response.body).to match(/Edit user/)
-        # response.should have_selector("title", :text => "Edit user")
       end
     end
 
-    describe 'success' do
+    context 'success' do
       before(:each) do
-        @attr = { name: 'New Name', email: 'user@example.org',
-                  password: 'barbaz', password_confirmation: 'barbaz' }
+        @attr = {
+          name: 'New Name',
+          email: 'user@example.org',
+          password: 'barbaz',
+          password_confirmation: 'barbaz'
+        }
       end
 
-      it "should change the user's attributes" do
+      it "changes the user's attributes" do
         put :update, id: @user, user: @attr
         @user.reload
-        @user.name.should  == @attr[:name]
-        @user.email.should == @attr[:email]
+        expect(@user.name).to eq @attr[:name]
+        expect(@user.email).to eq @attr[:email]
       end
 
-      it 'should redirect to the user show page' do
+      it 'redirects to the user show page' do
         put :update, id: @user, user: @attr
-        response.should redirect_to(user_path(@user))
+        expect(response).to redirect_to(user_path(@user))
       end
 
-      it 'should have a flash message' do
+      it 'displays a success message' do
         put :update, id: @user, user: @attr
-        flash[:success].should =~ /updated/
+        expect(flash[:success]).to match(/updated/)
       end
     end
   end
@@ -214,42 +222,42 @@ describe UsersController do
       @user = FactoryGirl.create(:user)
     end
 
-    describe 'for non-signed-in users' do
-      it "should deny access to 'edit'" do
+    context 'for non-signed-in users' do
+      it "denies access to 'edit'" do
         get :edit, id: @user
-        response.should redirect_to(signin_path)
+        expect(response).to redirect_to(signin_path)
       end
 
-      it "should deny access to 'update'" do
+      it "denies access to 'update'" do
         put :update, id: @user, user: {}
-        response.should redirect_to(signin_path)
+        expect(response).to redirect_to(signin_path)
       end
     end
 
-    describe 'for signed-in users' do
+    context 'for signed-in users' do
       before(:each) do
         wrong_user = FactoryGirl.create(:user, email: 'user@example.net')
         test_sign_in(wrong_user)
       end
 
-      it "should require matching users for 'edit'" do
+      it "requires matching users for 'edit'" do
         get :edit, id: @user
-        response.should redirect_to(root_path)
+        expect(response).to redirect_to(root_path)
       end
 
-      it "should require matching users for 'update'" do
+      it "requires matching users for 'update'" do
         put :update, id: @user, user: {}
-        response.should redirect_to(root_path)
+        expect(response).to redirect_to(root_path)
       end
     end
   end
 
-  describe "GET 'index'" do
-    describe 'for non-signed-in users' do
-      it 'should deny access' do
+  describe '.index' do
+    context 'for non-signed-in users' do
+      it 'denies access' do
         get :index
-        response.should redirect_to(signin_path)
-        flash[:notice].should =~ /sign in/i
+        expect(response).to redirect_to(signin_path)
+        expect(flash[:notice]).to match(/sign in/i)
       end
     end
 
@@ -265,26 +273,27 @@ describe UsersController do
         end
       end
 
-      it 'should be successful' do
+      it 'succeeds' do
         get :index
-        response.should be_success
+        expect(response).to be_success
       end
 
-      it 'should have the right title' do
+      # TODO: move or delete
+      it 'has the right title' do
         get :index
         expect(response.body).to match(/All users/)
-        # response.should have_selector("title", :text => "All users")
       end
 
-      it 'should have an element for each user' do
+      # TODO: move or delete
+      it 'has an element for each user' do
         get :index
         @users[0..2].each do |_user|
           expect(response.body).to match(/#{@base_title} | Home/)
-          # response.should have_selector 'li', :text => user.name
         end
       end
 
-      xit 'should paginate users' do
+      # TODO: move or delete
+      xit 'paginates users' do
         get :index
         response.should have_selector('div.pagination')
         response.should have_selector('span.disabled', text: 'Previous')
@@ -294,46 +303,46 @@ describe UsersController do
     end
   end
 
-  describe "DELETE 'destroy'" do
+  describe '.destroy' do
     before(:each) do
       @user = FactoryGirl.create(:user)
     end
 
-    describe 'as a non-signed-in user' do
-      it 'should deny access' do
+    context 'as a non-signed-in user' do
+      it 'denies access' do
         delete :destroy, id: @user
-        response.should redirect_to(signin_path)
+        expect(response).to redirect_to(signin_path)
       end
     end
 
-    describe 'as a non-admin user' do
-      it 'should protect the page' do
+    context 'as a non-admin user' do
+      it 'protects the page' do
         test_sign_in(@user)
         delete :destroy, id: @user
-        response.should redirect_to(root_path)
+        expect(response).to redirect_to(root_path)
       end
     end
 
-    describe 'as an admin user' do
+    context 'as an admin user' do
       before(:each) do
         admin = FactoryGirl.create(:user, email: 'admin@example.com', admin: true)
         test_sign_in(admin)
       end
 
-      it 'should destroy the user' do
-        lambda do
+      it 'destroys the user' do
+        expect do
           delete :destroy, id: @user
-        end.should change(User, :count).by(-1)
+        end.to change(User, :count).by(-1)
       end
 
-      it 'should redirect to the users page' do
+      it 'redirects to the users page' do
         delete :destroy, id: @user
-        response.should redirect_to(users_path)
+        expect(response).to redirect_to(users_path)
       end
 
-      it 'should not allow admin to delete himself' do
+      it 'does not allow admin to self-delete' do
         delete :destroy, id: @user
-        response.should redirect_to(users_path)
+        expect(response).to redirect_to(users_path)
       end
     end
   end
