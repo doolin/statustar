89a586fd8f8f18302062bf2a1f392ceffca5521d
David Doolin <david.doolin@gmail.com>
2017-03-31 13:40:58 -0700

    rails502 - permit relevant params for session
    Recall this code base is being updated from, essentially,
    rails 3.0 ~2010 railstutorial code. Rails 5 really is going
    to be a major advance here.
    
    For this commit, permitted params are partly working in the
    sessions controller. A little bit more work needs to be done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params[:session][:email],
-                             params[:session][:password])
+    # binding.pry
+    params.permit(:email, :password)
+    user = User.authenticate(params[:email], params[:password])
+
+    # user = User.authenticate
+
     if user.nil?
       flash.now[:error] = 'Invalid email/password combination.'
       @title = 'Sign in'
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe SessionsController do
   render_views
 
-  describe 'GET :new' do
+  describe '.new' do
     it 'succeeds' do
       get :new
       expect(response).to be_success
@@ -17,51 +17,51 @@ describe SessionsController do
     end
   end
 
-  describe 'POST :create' do
-    describe 'invalid signin' do
+  describe '.create' do
+    context 'with invalid signin' do
       before(:each) do
         @attr = { email: 'email@example.com', password: 'invalid' }
       end
 
       it 're-renders the new page' do
-        post :create, session: @attr
+        post :create, params: @attr
         expect(response).to render_template('new')
       end
 
-      it 'should have the right title' do
+      it 'has the right title' do
         post :create, session: @attr
         expect(response.body).to match(/Sign in/)
         # respsonse.should have_selector("title", :text => "Sign in")
       end
 
-      it 'should have a flash.now message' do
+      it 'displays flash message for invalid signin' do
         post :create, session: @attr
         # flash.now[:error].should =~ /invalid/i
         expect(flash.now[:error]).to match(/invalid/i)
       end
     end
 
-    describe 'with valid email and password' do
+    context 'with valid email and password' do
       before(:each) do
         @user = FactoryGirl.create(:user)
         @attr = { email: @user.email, password: @user.password }
       end
 
       it 'signs in the user' do
-        post :create, session: @attr
+        post :create, params: @attr
         expect(controller.current_user).to eq @user
         expect(controller).to be_signed_in
       end
 
-      it 'should redirect to the user show page' do
-        post :create, session: @attr
+      it 'redirects to the user show page' do
+        post :create, params: @attr
         expect(response).to redirect_to(user_path(@user))
       end
     end
   end
 
-  describe 'DELETE :destroy' do
-    it 'should sign a user out' do
+  describe '.destroy' do
+    it 'signs out user' do
       test_sign_in(FactoryGirl.create(:user))
       delete :destroy
       expect(controller).to_not be_signed_in
