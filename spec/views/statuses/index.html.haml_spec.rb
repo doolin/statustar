8564a4c881604adedde828c5b21be711f970d648
David Doolin <david.doolin@gmail.com>
2017-04-02 08:02:12 -0700

    rails502 - view specs now working

--- spec/views/statuses/index.html.haml_spec.rb
+++ spec/views/statuses/index.html.haml_spec.rb
@@ -2,8 +2,15 @@
 require 'spec_helper'
 
 describe 'statuses/index.html.haml' do
+  before do
+    allow(view).to receive(:title).and_return('Blah')
+    allow(view).to receive(:signed_in?).and_return(true)
+    allow(view).to receive(:current_user).and_return(create(:user))
+    allow(view).to receive(:logo).and_return('logo')
+  end
+
   # Fails with syntax error in statuses partial.
-  xit 'should render Show and Back text' do
+  xit 'renders Show and Back text' do
     render template: 'layouts/application.html.haml'
     render partial: 'statuses/form.html.haml'
     rendered.should =~ /Busy/
@@ -11,15 +18,15 @@ describe 'statuses/index.html.haml' do
     rendered.should =~ /Open/
   end
 
-  it 'should infer the controller path' do
+  it 'infers the controller path' do
     expect(controller.request.path_parameters[:controller]).to eq('statuses')
   end
 
-  it 'should infer the controller action' do
+  it 'infers the controller action' do
     expect(controller.request.path_parameters[:action]).to eq('index')
   end
 
-  it 'should display edit statuses' do
+  it 'displays edit statuses' do
     render template: 'layouts/application.html.haml'
     expect(rendered).to match(/Status/)
   end
