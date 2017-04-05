04121bfef96cb163a0b3252702942b200bf4e88e
David Doolin <david.doolin@gmail.com>
2017-04-05 08:09:59 -0700

    status request spec updated
    This was a next step in getting the specs aligned with rspec 3.
    The request specs may or may not end up being useful, but it was
    useful for me to build the spec, a good review of rails routing.
    Will probably revisit request specs in the near future.

--- spec/requests/statuses_spec.rb
+++ spec/requests/statuses_spec.rb
@@ -1,15 +1,51 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-# TODO: develop these specs along with the statuses_controller_spec
-
 describe 'Statuses' do
   let(:status) { create :status }
 
-  describe 'creation' do
-    it 'does not make a new status' do
+  describe 'new' do
+    it 'renders new template' do
+      get new_status_path
+      expect(response).to render_template('statuses/new')
+    end
+  end
+
+  describe 'show' do
+    it 'renders show template' do
       get status_path(status)
       expect(response).to render_template('statuses/show')
+    end
+  end
+
+  describe 'edit' do
+    it 'renders edit template' do
+      get edit_status_path(status)
+      expect(response).to render_template('statuses/edit')
+      expect(response).to render_template('statuses/_form')
+      expect(response).to render_template('layouts/statuses')
+    end
+  end
+
+  describe 'index' do
+    it 'renders index template' do
+      get statuses_path
+      expect(response).to render_template('statuses/index')
+      expect(response).to render_template('layouts/statuses')
+    end
+  end
+
+  describe 'delete' do
+    it 'renders delete template' do
+      delete status_path(status)
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe 'update' do
+    it 'renders update template' do
+      put status_path(status)
+      expect(response).to redirect_to(status_path(status))
     end
   end
 end
