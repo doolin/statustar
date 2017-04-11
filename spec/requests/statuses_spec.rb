ed3b547176923d49c88e8734a08948caad65877a
David Doolin <david.doolin@gmail.com>
2017-04-11 08:21:30 -0700

    status request specs updated
    Both signed in and signed out states are tested.
    
    There was a bit of weirdness with respect to the http
    post. The parameters need to be specified as strings
    instead of symbols. This is interesting, I should dig
    into how request and controller specs differ on this
    point.

--- spec/requests/statuses_spec.rb
+++ spec/requests/statuses_spec.rb
@@ -2,50 +2,102 @@
 require 'spec_helper'
 
 describe 'Statuses' do
+  include SessionsHelper
+
   let(:status) { create :status }
 
-  describe 'new' do
-    it 'renders new template' do
-      get new_status_path
-      expect(response).to render_template('statuses/new')
+  context 'signed in' do
+    before do
+      post sessions_path, "session" => { "email" => status.user.email, "password" => status.user.password }
+    end
+
+    describe 'new' do
+      it 'renders new template' do
+        get new_status_path
+        expect(response).to render_template('statuses/new')
+      end
+    end
+
+    describe 'show' do
+      it 'renders show template' do
+        get status_path(status)
+        expect(response).to render_template('statuses/show')
+      end
+    end
+
+    describe 'edit' do
+      it 'renders edit template' do
+        get edit_status_path(status)
+        expect(response).to render_template('statuses/edit')
+        expect(response).to render_template('statuses/_form')
+        expect(response).to render_template('layouts/statuses')
+      end
+    end
+
+    describe 'index' do
+      it 'renders index template' do
+        get statuses_path
+        expect(response).to render_template('statuses/index')
+        expect(response).to render_template('layouts/statuses')
+      end
+    end
+
+    describe 'delete' do
+      it 'renders delete template' do
+        delete status_path(status)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    describe 'update' do
+      it 'redirects to status' do
+        put status_path(status)
+        expect(response).to redirect_to(status_path(status))
+      end
     end
   end
 
-  describe 'show' do
-    it 'renders show template' do
-      get status_path(status)
-      expect(response).to render_template('statuses/show')
+  context 'signed out' do
+    describe 'new' do
+      it 'redirects to sign in' do
+        get new_status_path
+        expect(response).to redirect_to signin_path
+      end
     end
-  end
 
-  describe 'edit' do
-    it 'renders edit template' do
-      get edit_status_path(status)
-      expect(response).to render_template('statuses/edit')
-      expect(response).to render_template('statuses/_form')
-      expect(response).to render_template('layouts/statuses')
+    describe 'show' do
+      it 'renders show template' do
+        get status_path(status)
+        expect(response).to render_template('statuses/show')
+      end
     end
-  end
 
-  describe 'index' do
-    it 'renders index template' do
-      get statuses_path
-      expect(response).to render_template('statuses/index')
-      expect(response).to render_template('layouts/statuses')
+    describe 'edit' do
+      it 'redirects to sign in' do
+        get edit_status_path(status)
+        expect(response).to redirect_to signin_path
+      end
     end
-  end
 
-  describe 'delete' do
-    it 'renders delete template' do
-      delete status_path(status)
-      expect(response).to redirect_to(root_path)
+    describe 'index' do
+      it 'redirects to sign in' do
+        get statuses_path
+        expect(response).to redirect_to signin_path
+      end
     end
-  end
 
-  describe 'update' do
-    it 'renders update template' do
-      put status_path(status)
-      expect(response).to redirect_to(status_path(status))
+    describe 'delete' do
+      it 'redirects to sign in' do
+        delete status_path(status)
+        expect(response).to redirect_to signin_path
+      end
+    end
+
+    describe 'update' do
+      it 'redirects to sign in' do
+        put status_path(status)
+        expect(response).to redirect_to signin_path
+      end
     end
   end
 end
