7723aa905798ba9a775464e35ed405f64bfd5efe
David Doolin <david.doolin@gmail.com>
2017-04-25 06:01:04 -0700

    status _form spec working
    Once again, I realize view specs are horribly pass\'e,
    but these were already written, and I'm learning a lot about
    how to create the necessary doubles, and to reduce the amount
    of logic and variables in views going forward.
    
    In this particular commit, I was able to knock out the
    required code changes relatively quickly, around 20 minutes.
    I did a bit of rspec reading, and checked some previous
    commits, and like magic everything just worked.
    
    Almost done with all the pending specs for this project.
    Will start working on UI next.

--- spec/views/statuses/_form.html.haml_spec.rb
+++ spec/views/statuses/_form.html.haml_spec.rb
@@ -4,26 +4,27 @@ require 'spec_helper'
 
 describe 'statuses/_form.html.haml' do
   before(:each) do
-    @status = assign(:status, stub_model(Status,
-                                         state: 1,
-                                         user_id: 1))
-    render template: 'layouts/application', action: 'show'
+    @status = assign(:status, Status.new(state: 1, user_id: 1))
+    allow(view).to receive(:title)
+    allow(view).to receive(:signed_in?).and_return(true)
+    allow(view).to receive(:current_user).and_return(User.new)
+    render
   end
 
-  xit 'should render field and action tags' do
-    rendered.should have_selector 'div', class: 'field'
-    rendered.should have_selector 'div', class: 'actions'
+  it 'renders field and action tags' do
+    expect(rendered).to have_selector 'div', class: 'field'
+    expect(rendered).to have_selector 'div', class: 'actions'
   end
 
-  xit 'displays all the statuses' do
+  it 'displays all the statuses' do
     assign(:statuses, [
-             stub_model(Status, name: 'Busy'),
-             stub_model(Status, name: 'Maybe'),
-             stub_model(Status, name: 'Open')
+             Status.new(state: 'Busy'),
+             Status.new(state: 'Maybe'),
+             Status.new(state: 'Open')
            ])
-    # render
-    rendered.should =~ /Busy/
-    rendered.should =~ /Maybe/
-    rendered.should =~ /Open/
+
+    expect(rendered).to match(/Busy/)
+    expect(rendered).to match(/Maybe/)
+    expect(rendered).to match(/Open/)
   end
 end
