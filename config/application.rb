58d22c71325bcf87636fbeff83db101976a90c74
David Doolin <david.doolin@gmail.com>
2017-05-07 06:31:42 -0700

    rails51 - more app:update
    Chewing through the updates while ensuring the specs stay
    in passing mode. Couple of things:
    
    1. environments and defaults not handled yet.
    2. going forward rubocop should ignore some of the
    idiosyncrasies of the rails configuration and env
    files to make updating easier.

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 if defined?(Bundler)
   Bundler.require(:default, :assets, Rails.env)
