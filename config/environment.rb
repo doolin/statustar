7b49c27e9405fc175d607242d100a878df03d310
David Doolin <david.doolin@gmail.com>
2017-11-18 08:54:37 -0800

    railshead - sqlite3 default boolean change

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Upcoming deprecation in Rails 5.2
+Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+# Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = false
+
 # Initialize the Rails application.
 Rails.application.initialize!
