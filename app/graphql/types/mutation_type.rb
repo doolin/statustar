0aae823a62ae2e39b5c012681c73f9f5179aa1e1
dave doolin <david.doolin@gmail.com>
2018-09-23 15:25:58 -0700

    gql - add status mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :status, mutation: Mutations::Status
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
