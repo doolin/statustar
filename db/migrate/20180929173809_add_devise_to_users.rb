d95f3fbfb99789ef0d70af75c83d3f47ad4d53aa
dave doolin <david.doolin@gmail.com>
2018-09-29 10:44:55 -0700

    devise - migration added, specs and features pass

---
+++ db/migrate/20180929173809_add_devise_to_users.rb
@@ -0,0 +1,23 @@
+class AddDeviseToUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |t|
+      # t.change :email,              null: false, default: ""
+      t.string :password, null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
+
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      t.datetime :remember_created_at
+
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2011_10_05_152621) do
+ActiveRecord::Schema.define(version: 2018_09_29_173809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2011_10_05_152621) do
     t.string "encrypted_password"
     t.string "salt"
     t.boolean "admin", default: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
