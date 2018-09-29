c98221b357fb89b27a5083cceacf0ecfa5691681
dave doolin <david.doolin@gmail.com>
2018-09-29 09:24:00 -0700

    database cleaner configured

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,17 @@ RSpec.configure do |config|
   #   c.syntax = [:expect, :should]
   # end
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.include FactoryBot::Syntax::Methods
 
   # config.raise_errors_for_deprecations!
